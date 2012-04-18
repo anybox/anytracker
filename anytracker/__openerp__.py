1a25f4f59444c641745503a2ecaa8bcf5dcdcfee
Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
2012-04-18 11:12:50 +0000

    add access rules

--- anytracker/__openerp__.py
+++ anytracker/__openerp__.py
@@ -4,17 +4,18 @@
         'author' : 'cgoutte',
         'website' : 'http://anybox.fr',
         'category' : 'Generic Modules/Others',
-        'depends' : ['base'],
+        'depends' : [
+            'base',
+        ],
         'description' : 'Ticket Module',
-        'init_xml' : [
-                ],
+        'init_xml' : [],
         'demo_xml' : [],
         'update_xml' : [
-                'anytracker_view.xml',
-                'wizard/import_freemind_wizard.xml',
-                'ticket_data.xml'
-                ],
+            'security/ir.model.access.csv',
+            'anytracker_view.xml',
+            'wizard/import_freemind_wizard.xml',
+            'ticket_data.xml'
+        ],
         'active': False,
         'installable': True
 }
-
