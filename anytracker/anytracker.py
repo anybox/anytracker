3efc26605423e94e4a218630bb87f681a64343e4
Jean-Sebastien SUZANNE <jssuzanne@anybox.fr>
2012-04-19 10:14:49 +0000

    clean the code

--- anytracker/anytracker.py
+++ anytracker/anytracker.py
@@ -29,16 +29,17 @@ from tools.translate import _
 
 
 class complexity(osv.osv):
+
     _name = 'anytracker.ticket.complexity'
+
     _columns = {
         'name': fields.char('Name', size=3, required=True),
         'rating': fields.float('Rating of time taking'),
-        }
-
-complexity()
+    }
 
 
 class workflow1(osv.osv):
+
     _name = 'anytracker.ticket.workflow1'
 
     _columns = {
@@ -46,11 +47,10 @@ class workflow1(osv.osv):
         'state': fields.char('state', size=64, required=True)
     }
 
-workflow1()
 
+class anytracker_ticket_history(osv.osv):
 
-class ticket_history(osv.osv):
-    _name = 'ticket.history'
+    _name = 'anytracker.ticket.history'
     _description = 'History of ticket'
 
     _columns = {
@@ -61,7 +61,9 @@ class ticket_history(osv.osv):
 
 
 class ticket(osv.osv):
+
     _name = 'anytracker.ticket'
+
     _description = "Tickets for project management"
 
     def siblings(self, cr, uid, ids, field_name, args, context=None):
@@ -85,7 +87,6 @@ class ticket(osv.osv):
         'infos': fields.text('task description', required=False),
         'state': fields.char('state', 30, required=False),
         'siblings': fields.function(siblings, type='many2many', obj='anytracker.ticket', string='Siblings', method=True),
-        'projectroot': fields.boolean('is the root node', required=False),
         'duration': fields.selection([(0, '< half a day'), (None, 'Will be computed'), (1, 'Half a day')], 'duration'),
         'child_ids': fields.one2many('anytracker.ticket', 'parent_id', 'children', required=False),
         'assignedto_ids': fields.many2many('res.users', 'ticket_assignement_rel', 'ticket_id', 'user_id', required=False),
@@ -95,73 +96,13 @@ class ticket(osv.osv):
         'workflow_id': fields.many2one('anytracker.ticket.workflow1', 'kanban_status', required=True),
         'history_ids': fields.many2many('ticket.history', 'ticket_ticket_history_rel', 'ticket_id', 'history_id', 'History'),
     }
+
     #complexity should be a many2many table, so a as to make is possibilble for various users (assignees) to rate different tickets.
     _defaults = {
         'state': 'Analyse',
         'duration': 0,
         }
 
-    def _autoroot(self, cr, uid, ids, context=None):
-        """
-        eases the process of checking constraints so as to guess the only possible root when applies
-        """
-        orphans = self._orphans(cr, uid, ids, context=context)
-        roots = self._roots(cr, uid, ids, context=context)
-        if len(oprhans) == 1:
-            if not roots:
-                orphans[0].set_root()
-                return True
-        else:
-            return False
-
-    def _check_roots_orphans(self, cr, uid, ids, context=None):
-        roots = self._roots(cr, uid, ids, context)
-        orphans = self._orphans(cr, uid, ids, context)
-        if len(roots) == 1 and len(orphans) == 1:
-            if roots[0].id == orphans[0].id:
-                return True
-        return False
-
-    def _easy_check(self, cr, uid, context=None):
-        return self._check_root_orphans(cr, uid, context=None
-           ) or self._autoroot(cr, uid, context=None)
-
-    def link_to_parent(self, cr, uid, **kw):
-        pass
-
-    def link_to_child(self, cr, uid, **kw):
-        pass
-
-    def split(self, cr, uid, **kw):
-        pass
-
-    def _roots(self, cr, uid, ids, context=None):
-        nodes = self.browse(cr, uid, ids, context=context)
-        result = []
-        for node in nodes:
-            if node.projectroot:
-                result.append(node)
-        return result
-
-    def _orphans(self, cr, uid, ids, context=None):
-        nodes = self.browse(cr, uid, ids, context=context)
-        orphans = []
-        for node in nodes:
-            if not node.parents:
-                orphans.append(node)
-        return orphans
-
-    def _set_root(self, cr, uid, ids, context=None):
-        self.write(cr, uid, ids, {'root': True}, context=context)
-
-    def _check_roots(self, cr, uid, ids, context=None):
-        roots = _roots(self, cd, uid, ids, context=context)
-        if len(roots) == 1:
-            return True
-        else:
-            return False
-
-        #'duration': fields.selection([(0, '< half a day'), (None, 'Will be computed'), (1, 'Half a day')], 'duration'),
     def _add_history(self, cr, uid, values, context=None):
         vals = ""
         def _all2many(name, obj, val):
@@ -237,13 +178,5 @@ class ticket(osv.osv):
     def write(self, cr, uid, ids, values, context=None):
         values = self._add_history(cr, uid, values, context=context)
         return super(ticket, self).write(cr, uid, ids, values, context=context)
-
-#=========================================================================
-#Devrait renvoyer Bool,roots afin de de pas appeler deux fois _roots
-#une fois pour la verif et une fois pour savoir quels sont les noeuds
-#en cause
-#========================================================================
-
-ticket()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
