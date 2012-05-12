da0d383195e65db4ca251f9c4299682ebd90edbf
Christophe Combelles <ccomb@free.fr>
2012-05-12 22:01:23 +0200

    We don't need a copyright notice in every single file. Just kept in the __openerp__.py

--- anytracker/wizard/__init__.py
+++ anytracker/wizard/__init__.py
@@ -1,30 +1,3 @@
-##############################################################################
-#
-#    anytracker module for OpenERP, Ticket module
-#    Copyright (C) 2012 Anybox (<http://www.anybox.fr>)
-#                Colin GOUTTE <cgoute@anybox.fr>
-#                Christophe COMBELLES <ccomb@anybox.fr>
-#                Simon ANDRE <sandre@anybox.fr>
-#                Jean Sebastien SUZANNE <jssuzanne@anybox.fr>
-#
-#    This file is a part of anytracker
-#
-#    anytracker is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    anytracker is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 import import_freemind_wizard
 import export_freemind_wizard
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- anytracker/wizard/import_freemind_wizard.py
+++ anytracker/wizard/import_freemind_wizard.py
@@ -1,30 +1,4 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    anytracker module for OpenERP, Ticket module
-#    Copyright (C) 2012 Anybox (<http://www.anybox.fr>)
-#                Colin GOUTTE <cgoute@anybox.fr>
-#                Christophe COMBELLES <ccomb@anybox.fr>
-#                Simon ANDRE <sandre@anybox.fr>
-#                Jean Sebastien SUZANNE <jssuzanne@anybox.fr>
-#
-#    This file is a part of anytracker
-#
-#    anytracker is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    anytracker is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 from osv import osv, fields
 from tools.translate import _
 from xml import sax
