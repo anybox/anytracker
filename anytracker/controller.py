16dec1c9407a98e7e63cd1332672a0a4e71f169c
Christophe Combelles <ccomb@free.fr>
2013-10-22 15:55:00 +0200

    Attemps to fix the permalink redirect

--- anytracker/controller.py
+++ anytracker/controller.py
@@ -1,8 +1,10 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
 from openerp.addons.web import http
 import werkzeug.utils
-from openerp import pooler, SUPERUSER_ID
+from openerp import pooler, SUPERUSER_ID as uid
+from urlparse import urljoin
+from urllib import urlencode
 
 
 class UrlDirection(http.Controller):
@@ -10,6 +12,7 @@ class UrlDirection(http.Controller):
 
     @http.httprequest
     def __ticket(self, request):
+        # extract db and ticket number from URL
         path = request.httprequest.path[1:].split('/')
         url = "/web/webclient/home/#"
         if len(path) != 4 or path[2] != 'ticket':
@@ -19,31 +22,27 @@ class UrlDirection(http.Controller):
             pool = pooler.get_pool(db)
         except Exception, e:
             return "%r" % (e,)
-        ticket = path[-1]
-        cursor = pooler.get_db(db).cursor()
+
+        # retrieve the ticket
+        number = path[-1]
+        cr = pooler.get_db(db).cursor()
         ticket_obj = pool.get('anytracker.ticket')
-        domain = [
-            ('number', '=', int(ticket)),
-        ]
-        ticket_ids = ticket_obj.search(cursor, SUPERUSER_ID, domain)
+        ticket_ids = ticket_obj.search(cr, uid, [('number', '=', int(number))])
         if not ticket_ids:
-            return "Bad ticket number %r" % ticket
-        data_obj = pool.get('ir.model.data')
-        action_id = data_obj.get_object_reference(
-            cursor, SUPERUSER_ID, 'anytracker', 'act_all_tasks')[1]
-        val = [
-            ('id',  str(ticket_ids[0])),
-            ('view_type', 'page'),
-            ('model', 'anytracker.ticket'),
-            ('action_id', str(action_id)),
-        ]
-        # FIXME: just below use urllib.urlencode
-        url += '&'.join(v[0] + "=" + v[1] for v in val)
+            return "Bad ticket number %r" % number
+        ticket_id = ticket_ids[0]
+
+        # contruct the new URL
+        query = {'db': db}
+        fragment = {
+            'model': 'anytracker.ticket',
+            'id': str(ticket_id),
+        }
+        base_url = pool.get('ir.config_parameter').get_param(cr, uid, 'web.base.url')
+        url = urljoin(base_url, "?%s#%s" % (urlencode(query), urlencode(fragment)))
         redirect = werkzeug.utils.redirect(url, 302)
         redirect.autocorrect_location_header = False
         return redirect
 
     def __getattr__(self, func_name):
         return self.__ticket
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
