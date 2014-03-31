579382163f78456fdec7eb84bbb276a57e634d57
Christophe Combelles <ccomb@free.fr>
2014-03-31 11:31:50 +0300

    syntax and scope fixes

--- anytracker/static/src/js/anytracker.js
+++ anytracker/static/src/js/anytracker.js
@@ -1,7 +1,7 @@
 openerp.anytracker = function(instance) {
     // instance is the main openerp object created, which contains all modules
     // we load the web module
-    module = instance.web
+    module = instance.web;
     module.ViewManagerAction.include({
         set_title: function() {
             // Replace the OpenERP breadcrumb with the Anytracker breadcrumb
@@ -11,12 +11,11 @@ openerp.anytracker = function(instance) {
                 || !this.action.context.active_id) {
                 return this._super();
             } else {
-                this.breadcrumbs = [];
-                id = this.action.context.active_id;
+                var id = this.action.context.active_id;
                 var self = this;
                 var tickets = new module.Model("anytracker.ticket");
                 tickets.call('get_breadcrumb', [[id]]).done(function(result) {
-                    var items = result[id]
+                    var items = result[id];
                     var breadcrumbs = [];
                     for (var i=0; i < items.length; i++) {
                         breadcrumbs.push(_.str.sprintf('<span class="oe_breadcrumb_item">%s</span>', items[i].name))
