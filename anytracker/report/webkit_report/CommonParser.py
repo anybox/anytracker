db08a20f298c1f6f5908bdbea1441cd4cab28bf2
Pierre Verkest <pverkest@anybox.fr>
2014-11-29 00:46:51 +0100

    [report] Parser refactor

---
+++ anytracker/report/webkit_report/CommonParser.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from openerp.report import report_sxw
+from tools.translate import _
+from datetime import datetime
+from openerp.tools import misc
+from osv import osv
+import pytz
+
+
+class CommonParser(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(CommonParser, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({'displayLocaleDateTime': self._displayLocaleDateTime,
+                                  })
+
+    def _displayLocaleDateTime(self, timezone, format='%d/%m/%Y %H:%M', server_tz=None):
+        try:
+            if not server_tz:
+                server_tz = misc.get_server_timezone()
+            return pytz.timezone(server_tz).localize(datetime.now()).astimezone(
+                pytz.timezone(timezone)).strftime(format)
+        except:
+            raise osv.except_osv(_('Timezone error or date format invalid!'),
+                                 _('Please set your timezone before to generate this report.') +
+                                 _(' One of those values is not valid: timezone= %r - format= %r')
+                                 % (timezone, format))
--- anytracker/report/webkit_report/ticket.py
+++ anytracker/report/webkit_report/ticket.py
@@ -1,30 +1,14 @@
 # -*- coding: utf-8 -*-
 from openerp.report import report_sxw
-from tools.translate import _
-from datetime import datetime
-from openerp.tools import misc
-from osv import osv
-import pytz
+from .CommonParser import CommonParser
 
 
-class TicketReport(report_sxw.rml_parse):
+class TicketReport(CommonParser):
     def __init__(self, cr, uid, name, context):
         super(TicketReport, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({'test_methode': self._test_methode,
                                   'displayLocaleDateTime': self._displayLocaleDateTime,
                                   })
-
-    def _displayLocaleDateTime(self, timezone, format='%d/%m/%Y %H:%M', server_tz=None):
-        try:
-            if not server_tz:
-                server_tz = misc.get_server_timezone()
-            return pytz.timezone(server_tz).localize(datetime.now()).astimezone(
-                pytz.timezone(timezone)).strftime(format)
-        except:
-            raise osv.except_osv(_('Timezone error or date format invalid!'),
-                                 _('Please set your timezone before to generate this report.') +
-                                 _(' One of those values is not valid: timezone= %r - format= %r')
-                                 % (timezone, format))
 
     def _test_methode(self):
         return False
