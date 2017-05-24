37a7e414a2810d8a29ad5640f29978b7d0bcef2d
Christophe Combelles <ccomb@free.fr>
2017-05-25 00:49:32 +0200

    fixed bug deadline not appearing

--- anytracker/priority/priority.py
+++ anytracker/priority/priority.py
@@ -60,9 +60,8 @@ class Ticket(models.Model):
     _inherit = 'anytracker.ticket'
 
     has_deadline = fields.Boolean(
-        'priority_id.deadline',
-        readonly=True,
-        type="boolean")
+        related='priority_id.deadline',
+        readonly=True)
     deadline = fields.Date(
         'Deadline')
     priority_id = fields.Many2one(
